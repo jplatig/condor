2cd5af1e3cdc4a51803f4151717531a10cc9a11b
Albert Young <albert@alberts-mbp-2.dfci.harvard.edu>
2015-10-15 18:03:07 -0400

    replaced p-values in condor.compare.qscores with permutation p-values

--- R/condor.compare.qscores.R
+++ R/condor.compare.qscores.R
@@ -6,17 +6,18 @@
 #' @param z Output of \code{\link{condor.plot.community.overlap}}
 #' @param lab.x Label of first condition
 #' @param lab.y Label of second condition
+#' @param nsamp Number of permutation tests to run, passed to \code{\link{condor.core.enrich}}
 #' @return ggplot object
 #' @import igraph
 #' @import Matrix
 #' @export
 #' 
-condor.compare.qscores <- function(x, y, z, lab.x="Condition 1", lab.y="Condition 2") {
+condor.compare.qscores <- function(x, y, z, lab.x="Condition 1", lab.y="Condition 2", nsamp=1000) {
   qscore <- merge(x, y, by="names")
-  com.map <- apply(r, 1, function(x){which(x==max(x))})
+  com.map <- apply(z, 1, function(x){which(x==max(x))})
   # get all stable genes
   stable.genes <- c()
-  for (i in 1:nrow(r)) {
+  for (i in 1:nrow(z)) {
     stable.x <- subset(x, com==i)$names
     stable.y <- subset(y, com%in%com.map[[i]])$names
     stable.genes <- c(stable.genes, intersect(stable.x, stable.y))
@@ -24,13 +25,12 @@ condor.compare.qscores <- function(x, y, z, lab.x="Condition 1", lab.y="Conditio
   qscore$stable <- with(qscore, names %in% stable.genes)
   d <- melt(qscore, measure.vars = c("Q.x", "Q.y"))
   
-  x.p <- wilcox.test(value ~ stable, data=subset(d, variable=="Q.x"))$p.value
-  y.p <- wilcox.test(value ~ stable, data=subset(d, variable=="Q.y"))$p.value
+  x.p <- condor.core.enrich(stable.genes, x, perm=TRUE, nsamp=nsamp)$perm.pvals
+  y.p <- condor.core.enrich(stable.genes, x, perm=TRUE, nsamp=nsamp)$perm.pvals
   
-  x.ks <- ks.test(subset(qscore, stable==T)$Q.x, subset(qscore, stable==F)$Q.x, alternative="less")$p.value
-  y.ks <- ks.test(subset(qscore, stable==T)$Q.y, subset(qscore, stable==F)$Q.y, alternative="less")$p.value
-  
-  df <- data.frame(variable=c("Q.x", "Q.y"), p=c(x.p, y.p), ks=c(x.ks, y.ks))
+  df <- data.frame(variable=c("Q.x", "Q.y"),
+                   p=c(x.p$wilcox.perm, y.p$wilcox.perm),
+                   ks=c(x.p$ks.perm, y.p$ks.perm))
   d <- merge(d, df, by="variable")
   
   d$p <- paste("Wilcox p:", format(d$p, scientific=T, digits=2))

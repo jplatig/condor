6dc9e169dc9ca170af185e1867ba9d974c8af8e2
Albert Young <albert@alberts-mbp-206.wireless.dartmouth.edu>
2016-05-08 18:17:27 -0400

    updated condor.compare.qscores with scale_y_log10()

--- R/condor.compare.qscores.R
+++ R/condor.compare.qscores.R
@@ -4,13 +4,13 @@
 #' @param x first output of \code{\link{condor.qscore}}
 #' @param y second output of \code{\link{condor.qscore}}
 #' @param by character string indicating whether to determine stability by 'row', 'column', or 'both'
+#' @param seed - random seed
 #' @param label.x Label of first condition
 #' @param label.y Label of second condition
 #' @param red.name Name for red nodes
 #' @param blue.name Name for blue nodes
-#' @param scale.log TRUE/FALSE - If TRUE, plot log core scores
+#' @param scale.log TRUE/FALSE - If TRUE, plot log10 core scores
 #' @param nsamp Number of permutation tests to run, passed to \code{\link{condor.core.enrich}}
-#' @param plot.type character string indicating whether to plot violin plots or points
 #' @param node.type character string indicating whether to plot core score comparisons for red nodes, blue nodes, or both
 #' @param notch TRUE/FALSE - Add notches to boxplots
 #' @return ggplot object
@@ -18,16 +18,14 @@
 #' @import Matrix
 #' @export
 #' 
-condor.compare.qscores <- function(x, y, by=c("column","row","both"),
+condor.compare.qscores <- function(x, y, by=c("column","row","both"), seed=8518,
                                    label.x="Condition x", label.y="Condition y",
-                                   red.name="red", blue.name="blue", scale.log=FALSE,
-                                   nsamp=1e4, plot.type=c("violin", "points"),
-                                   node.type=c("both","red", "blue"), notch=TRUE) {
+                                   red.name="red", blue.name="blue", scale.log=TRUE,
+                                   nsamp=1e4, node.type=c("both","red", "blue"), notch=TRUE) {
   by <- match.arg(by)
-  plot.type <- match.arg(plot.type)
   node.type <- match.arg(node.type)
+  set.seed(seed)
   
-  ylabel <- ifelse(scale.log, "log(core score)", "Core score")
   qscore.red <- calc.qscore.stability(x, y, type="red", by=by)
   qscore.blue <- calc.qscore.stability(x, y, type="blue", by=by)
   
@@ -71,31 +69,18 @@ condor.compare.qscores <- function(x, y, by=c("column","row","both"),
   if (nrow(d)==0) {
     stop("No reds nor blues are assigned to different communities between conditions. Exiting.")
   }
-  
+
+  p <- ggplot(d, aes(x=variable, y=value, color=is.stable, fill=is.stable, label=p), environment=environment()) +
+    geom_boxplot(width=0.1, notch=notch, outlier.colour=NA, position=position_dodge(width=0.8), fill="white") +
+    geom_violin(position=position_dodge(width=0.8), adjust=0.5, alpha=0.3) +
+    scale_color_manual(name="", labels=c("Variable", "Stable"), values=c("seagreen4", "gray20"), guide=FALSE) +
+    scale_x_discrete(labels=c(label.x, label.y)) +
+    scale_fill_manual(name="", labels=c("Variable", "Stable"), values=c("seagreen4", "gray20")) +
+    xlab("Condition") + ylab("Core score") +
+    geom_text(data=d[!duplicated(paste0(d$variable, d$type)),], aes(x=variable, y=max(d$value) + 1), vjust=1, position=position_dodge(width=0.9), size=10*5/14) +
+    facet_grid(type~., scales="free")
   if (scale.log) {
-    d$value <- log(d$value)
+    p <- p + scale_y_log10(breaks=10^seq(-20,0,1))
   }
-  if (plot.type=="violin") {
-    ggplot(d, aes(x=variable, y=value, color=is.stable, fill=is.stable, label=p), environment=environment()) +
-      geom_boxplot(width=0.1, notch=notch, outlier.colour=NA, position=position_dodge(width=0.8), fill="white") +
-      geom_violin(position=position_dodge(width=0.8), adjust=0.5, alpha=0.3) +
-      scale_color_manual(name="", labels=c("Variable", "Stable"), values=c("seagreen4", "gray20"), guide=FALSE) +
-      scale_x_discrete(labels=c(label.x, label.y)) +
-      scale_fill_manual(name="", labels=c("Variable", "Stable"), values=c("seagreen4", "gray20")) +
-      xlab("Condition") + ylab(ylabel) +
-      geom_text(data=d[!duplicated(paste0(d$variable, d$type)),], aes(x=variable, y=max(d$value) + 1), vjust=1, position=position_dodge(width=0.9), size=10*5/14) +
-      facet_grid(type~., scales="free")
-  } else {
-    ggplot(d, aes(x=variable, y=value, fill=is.stable, color=is.stable, label=p), environment=environment()) +
-      geom_boxplot(fill="white", outlier.colour=NA, notch=notch,
-                   position=position_dodge(width=0.9)) +
-      geom_point(position=position_jitterdodge(dodge.width=0.9), alpha=0.3) +
-      scale_color_manual(name="", labels=c("Variable", "Stable"), values=c("seagreen4", "gray20")) +
-      scale_fill_discrete(guide=FALSE) +
-      scale_x_discrete(labels=c(label.x, label.y)) +
-      xlab("Condition") + ylab(ylabel) +
-      ggtitle("Core score distributions by condition and stability") +
-      geom_text(data=d[!duplicated(paste0(d$variable, d$type)),], aes(x=variable, y=max(d$value)), vjust=1, position=position_dodge(width=0.9), size=10*5/14) +
-      facet_grid(type~., scales="free")
-  }
+  p
 }

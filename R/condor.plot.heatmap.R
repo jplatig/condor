0934743f18ecc4ea9df802bfc95575b1ebbb6094
Albert Young <albert@alberts-mbp-206.wireless.dartmouth.edu>
2016-05-22 22:16:25 -0400

    fix to condor.plot.heatmap with colors, still not working with large data sets

--- R/condor.plot.heatmap.R
+++ R/condor.plot.heatmap.R
@@ -36,13 +36,21 @@ condor.plot.heatmap = function(condor.object, ticks=c("names", "coms", "none"), 
   if (is.null(get.edge.attribute(condor.object$G, "weight"))) {
     condor.object$G <- set.edge.attribute(condor.object$G, "weight", value=1)
   }
-  all.links <- cbind(get.edgelist(condor.object$G),get.edge.attribute(condor.object$G,"weight"))
-  all.links <- data.frame(all.links)
-  colnames(all.links) <- c("Var1","Var2","value")
-  # reorder reds according to community membership
   reds <- as.character(condor.object$red.memb[order(condor.object$red.memb[,2]),1])
-  # reorder blues according to community membership
   blues <- as.character(condor.object$blue.memb[order(condor.object$blue.memb[,2]),1])
+  if (add.color) {
+    # convert edge lists to adjacency matrices (n reds x m blues)
+    # plotting colors requires a complete edgelist
+    adj <- get.adjacency(condor.object$G, attr="weight", sparse=FALSE)
+    # reorder reds according to community membership
+    adj <- adj[reds,]
+    # reorder blues according to community membership
+    adj <- adj[,blues]
+    all.links <- melt(adj)
+  } else {
+    all.links <- cbind(data.frame(get.edgelist(condor.object$G)),get.edge.attribute(condor.object$G,"weight"))
+    colnames(all.links) <- c("Var1","Var2","value")
+  }
   
   red.sizes <- as.vector(table(condor.object$red.memb[,2]))
   cum.red.sizes <- cumsum(red.sizes)
@@ -53,46 +61,11 @@ condor.plot.heatmap = function(condor.object, ticks=c("names", "coms", "none"), 
   # reorder by community assignment
   all.links$Var1 <- factor(all.links$Var1,levels=unique(reds))
   all.links$Var2 <- factor(all.links$Var2,levels=unique(blues))
-  # change factor to numeric
-  all.links$value <- as.numeric(all.links$value)
   
   # get axis tick mark locations
   xbreaks <- blues[c(1, cum.blue.sizes[-length(cum.blue.sizes)] + 1)]
   ybreaks <- reds[c(1, cum.red.sizes[-length(cum.red.sizes)] + 1)]
 
-  if (is.null(ylabels)) {
-    ylabels <- unique(as.character(sort(condor.object$red.memb[,2])))
-    # remove y-axis tick marks that may cause overcrowding
-    min.sep <- max(cum.red.sizes) * tick.min.prop
-    last.tick.set <- 1
-    rm.idx <- c()
-    for (i in 1:(length(red.sizes)-1)) {
-      if (sum(red.sizes[last.tick.set:i]) > min.sep) {
-        last.tick.set <- i + 1
-      } else {
-        # remove next tick
-        rm.idx <- c(rm.idx, i + 1)
-      }
-    }
-    ylabels[rm.idx] <- ""
-  }
-  if (is.null(xlabels)) {
-    xlabels <- unique(as.character(sort(condor.object$blue.memb[,2])))
-    # remove x-axis tick marks that may cause overcrowding
-    min.sep <- max(cum.blue.sizes) * tick.min.prop
-    last.tick.set <- 1
-    rm.idx <- c()
-    for (i in 1:(length(blue.sizes)-1)) {
-      if (sum(blue.sizes[last.tick.set:i]) > min.sep) {
-        last.tick.set <- i + 1
-      } else {
-        # remove next tick
-        rm.idx <- c(rm.idx, i + 1)
-      }
-    }
-    xlabels[rm.idx] <- ""
-  }
-  
   p <- ggplot() +
     geom_tile(data=all.links, aes(Var2, Var1, fill=value))
   # plot heatmap
@@ -104,6 +77,38 @@ condor.plot.heatmap = function(condor.object, ticks=c("names", "coms", "none"), 
           panel.grid.minor = element_blank(),
           panel.background = element_blank())
   if (ticks=="coms") {
+    if (is.null(ylabels)) {
+      ylabels <- unique(as.character(sort(condor.object$red.memb[,2])))
+      # remove y-axis tick marks that may cause overcrowding
+      min.sep <- max(cum.red.sizes) * tick.min.prop
+      last.tick.set <- 1
+      rm.idx <- c()
+      for (i in 1:(length(red.sizes)-1)) {
+        if (sum(red.sizes[last.tick.set:i]) > min.sep) {
+          last.tick.set <- i + 1
+        } else {
+          # remove next tick
+          rm.idx <- c(rm.idx, i + 1)
+        }
+      }
+      ylabels[rm.idx] <- ""
+    }
+    if (is.null(xlabels)) {
+      xlabels <- unique(as.character(sort(condor.object$blue.memb[,2])))
+      # remove x-axis tick marks that may cause overcrowding
+      min.sep <- max(cum.blue.sizes) * tick.min.prop
+      last.tick.set <- 1
+      rm.idx <- c()
+      for (i in 1:(length(blue.sizes)-1)) {
+        if (sum(blue.sizes[last.tick.set:i]) > min.sep) {
+          last.tick.set <- i + 1
+        } else {
+          # remove next tick
+          rm.idx <- c(rm.idx, i + 1)
+        }
+      }
+      xlabels[rm.idx] <- ""
+    }
     p <- p + scale_y_discrete(breaks=ybreaks, labels=ylabels) +
       scale_x_discrete(breaks=xbreaks, labels=xlabels)
   }
@@ -122,9 +127,9 @@ condor.plot.heatmap = function(condor.object, ticks=c("names", "coms", "none"), 
     p <- p + coord_flip()
   }
   if (add.color) {
-    adj <- get.adjacency(condor.object$G, attr="weight", sparse=FALSE)
-    adj <- adj[reds,]
-    adj <- adj[,blues]
+    if (transpose) {
+      adj <- t(adj)
+    }
     
     combo.grob <- ggplotGrob(p)
     # get subset of adj that contains within-community links
@@ -133,59 +138,44 @@ condor.plot.heatmap = function(condor.object, ticks=c("names", "coms", "none"), 
     row.indices <- c()
     col.indices <- c()
     
+    # get fill values of  ggplot internals
+    rect <- combo.grob$grobs[[4]]$children[[2]]
+    fills <- rect$gp$fill
+    # reorder matrix
+    d <- data.frame(x=as.numeric(rect$x),y=as.numeric(rect$y))
+    d$n <- 1:nrow(d)
+    d <- d[with(d,order(x,y)),]
+    fills <- matrix(fills[d$n],nrow=nrow(adj))
+    
     if (is.null(colors)) {
       # cool tutorial http://novyden.blogspot.com/2013/09/how-to-expand-color-palette-with-ggplot.html
       getPalette <- colorRampPalette(brewer.pal(8, "Set1"))
       colors <- getPalette(ncom)
     }
-    for (i in 1:ncom) {
-      m <- matrix(0, nrow=nrow(adj), ncol=ncol(adj))
-      rownames(m) <- rownames(adj)
-      colnames(m) <- colnames(adj)
-      m[row.idx[i]:cum.red.sizes[i], col.idx[i]:cum.blue.sizes[i]] <- adj[row.idx[i]:cum.red.sizes[i], col.idx[i]:cum.blue.sizes[i]]
-      # hack to ensure consistent color scaling
-      if (i != ncom) {
-        m[nrow(m), ncol(m)] <- max(adj)  
+    # convert adjacency matrix values to hex colors
+    ConvertMatToHex <- function(mat, xs, ys, color) {
+      max.val <- max(mat)
+      mat <- mat[xs, ys]
+      if (is.null(dim(mat))) {
+        sapply(mat,function(x){
+          tint <- toupper(format(as.hexmode(round(x/max.val*255)),2))
+          paste0(color,tint)
+        }) 
       } else {
-        m[1, 1] <- max(adj)
+        apply(mat,2,function(x){
+          tint <- toupper(format(as.hexmode(round(x/max.val*255)),2))
+          paste0(color,tint)
+        })
       }
-      com.links <- melt(m)
-      colnames(com.links)[1:2] <- c("Var1", "Var2")
-      com.links$Var1 <- factor(com.links$Var1,levels=unique(com.links$Var1))
-      com.links$Var2 <- factor(com.links$Var2,levels=unique(com.links$Var2))
-      p2 <- ggplot() +
-        geom_tile(data=com.links, aes(Var2, Var1, fill=value))
-      if (ticks=="coms") {
-        p2 <- p2 + scale_y_discrete(breaks=ybreaks, labels=ylabels) +
-          scale_x_discrete(breaks=xbreaks, labels=xlabels)
-      }
-      if (ticks=="names") {
-        p2 <- p2 + theme(axis.text.x=element_text(angle = 45, hjust = 1))
-      }
-      if (ticks=="none") {
-        p2 <- p2 + theme(axis.ticks=element_blank(), axis.text.x=element_blank(), axis.text.y=element_blank())
-      }
-      if (log) {
-        p2 <- p2 + scale_fill_gradient(name="log10(edge weight)", low="white", high=colors[i], trans="log10", na.value="white")
-      } else {
-        p2 <- p2 + scale_fill_gradient(name="edge weight", low="white", high=colors[i], na.value="white")
-      }
-      if (transpose) {
-        p2 <- p2 + coord_flip()
-      }
-      p2g <- ggplotGrob(p2)
-      # change white to transparent
-      p2g$grobs[[4]]$children[[2]]$gp$fill <- gsub("#FFFFFFFF", "#00000000", p2g$grobs[[4]]$children[[2]]$gp$fill)
-      if (i == ncom) {
-        p2g$grobs[[4]]$children[[2]]$gp$fill[1] <- "#00000000"
-      } else {
-        p2g$grobs[[4]]$children[[2]]$gp$fill[length(p2g$grobs[[4]]$children[[2]]$gp$fill)] <- "#00000000"
-      }
-      combo.grob$grobs[[4]] <- addGrob(combo.grob$grobs[[4]],
-                                       getGrob(p2g$grobs[[4]],
-                                               "geom_rect.rect",
-                                               grep=TRUE))
     }
+    for (i in 1:ncom) {
+      xs <- row.idx[i]:cum.red.sizes[i]
+      ys <- col.idx[i]:cum.blue.sizes[i]
+      fills[xs, ys] <- ConvertMatToHex(adj, xs, ys, colors[i])
+    }
+    # reorder
+    fills <- as.vector(fills)[match(1:nrow(d),d$n)]
+    combo.grob$grobs[[4]]$children[[2]]$gp$fill <- fills
     plot.new()
     grid.draw(combo.grob)
   } else {
